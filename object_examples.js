79c7807becc8aeb7401a5f235734d9ac46c06e7b
George Lemmon <george.lemmon@gmail.com>
2021-10-20 20:40:55 -0600

    Add examples of objects to object_examples.js

---
+++ object_examples.js
@@ -0,0 +1,39 @@
+//Car
+
+var wheels = 4
+var doors = 4
+var liscensePlate = '234xyz'
+
+var car = {
+  wheels: 4,
+  doors: 4,
+  liscensePlate: '234xyz'
+}
+
+//Animal
+
+var legs = 2
+var arms = 'tiny'
+var teeth = 'huge'
+var extant = false
+
+var tRex = {
+  legs: 2,
+  arms: 'tiny',
+  teeth: 'huge',
+  extant: false
+}
+
+//Customer
+
+var attitude = 'pushy'
+var hair = 'bad'
+var motto = 'Live, laugh, love'
+var name = 'Karen'
+
+var customer = {
+  attitude: 'pushy',
+  hair: 'bad',
+  motto: 'Live, laugh, love',
+  name: 'Karen'
+}
